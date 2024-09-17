9f18fe602011a187f6867456b6288ef562dfc6af
Yauheni <yauhenbelarus@gmail.com>
2024-09-17 20:34:37 +0300

    added config, Dockerfile and docker-compose

---
+++ generate/cmd/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"generate/internal/config"
+	"log/slog"
+	"net/http"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("Starting application",
+		slog.String("env", cfg.Env),
+		slog.Any("cfg", cfg),
+	)
+
+	server := &http.Server{
+		Addr: ":8088",
+	}
+	server.ListenAndServe()
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
